ebc519b3042753f24ab70c099dec6fa20fbf7db3
Soumya <soumyaranjanbehura27@gmail.com>
2026-09-26 23:58:52 +0530

    updating the fakestore page

--- src/scripts/fakestore.js
+++ src/scripts/fakestore.js
@@ -4,7 +4,7 @@ function loadcategories(){
         return response.json();
     })
     .then(function(categories){
-        categories.unshift("All");
+        categories.unshift("all");
            console.log(categories);
        categories.map(function(category){
      
@@ -20,18 +20,19 @@ function bodyload(){
     loadproducts(`https://fakestoreapi.com/products`);
 }
 function loadproducts(url){
+    document.querySelector("main").innerHTML = "";
      fetch(url)
      .then(function(response){
         return response.json();
      })
      .then(function(products){
-        products.map(function(product){
+        products.map(function(product){  
         var div=document.createElement("div");
         div.className="card m-2 p-2";
         div.style.width="200px";
         div.innerHTML=` 
-         <img class="card-img-top" height="120" src=${product.image}> 
-         <div class="card-header" style="height:100px;">
+         <img class="card-img-top" height="120" width="200px" src=${product.image}> 
+         <div class="card-header" style="height:130px;">
          ${product.title}
          </div>
          <div class="card-body">
@@ -43,7 +44,7 @@ function loadproducts(url){
          </dl>
          </div>
          <div class="card-footer">
-         <button class="btn btn-warning w-100 bi bi-cart4" onclick="addclick()">Add to Cart</button>
+         <button class="btn btn-warning w-100 bi bi-cart4" onclick="addclick(${product.id})">Add to Cart</button>
          </div>
         `;
         document.querySelector("main").appendChild(div);
@@ -51,3 +52,35 @@ function loadproducts(url){
       
      })
     }
+function selectchange(){
+    var categoryname=document.getElementById("lstcategories").value;
+    if (categoryname==='all'){
+        loadproducts('https://fakestoreapi.com/products/');
+    }else{
+        console.log(`https://fakestoreapi.com/products/category/${categoryname}`);
+         loadproducts(`https://fakestoreapi.com/products/category/${categoryname}`);
+    }
+}
+function searchclick(){
+    var categoryName=document.getElementById("txtsearch").value;
+    if(categoryName===""){
+        loadproducts('https://fakestoreapi.com/products');
+    }else{
+        loadproducts(`https://fakestoreapi.com/products/category/${categoryName}`);
+    }
+}
+var cartitem=[];
+function getcartcount(){
+    document.getElementById("lblcount").innerHTML=cartitem.length;
+}
+function addclick(id){
+    fetch(`https://fakestoreapi.com/products/${id}`)
+    .then (function(response){
+        return response.json();
+    })
+    .then (function(product){
+        cartitem.push(product);
+        alert(`${product.title}\nAdded to Cart`);
+        getcartcount();
+    })
+}
